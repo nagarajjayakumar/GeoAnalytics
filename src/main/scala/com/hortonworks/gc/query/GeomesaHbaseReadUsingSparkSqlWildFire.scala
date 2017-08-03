9111dd8c5b7de14b5ea13416f6305055f304e3e6
nagarajjayakumar <nagaraj.jayakumar@gmail.com>
2017-08-03 10:29:20 -0400

    USE CASE 4

--- src/main/scala/com/hortonworks/gc/query/GeomesaHbaseReadUsingSparkSqlWildFire.scala
+++ src/main/scala/com/hortonworks/gc/query/GeomesaHbaseReadUsingSparkSqlWildFire.scala
@@ -7,7 +7,7 @@ object GeomesaHbaseReadUsingSparkSqlWildFire {
   // works for latest version of Geomesa 1.3.2 + Spark 2.x
 
   // specify the params for the datastore
-  val dsParams = Map("bigtable.table.name" -> "siteexposure")
+  val dsParams = Map("bigtable.table.name" -> "siteexposure_1M")
 
   // matches the params in the datastore loading code
   val featureTypeName:String = "event"
@@ -26,7 +26,7 @@ object GeomesaHbaseReadUsingSparkSqlWildFire {
 
     val dataFrame = sparkSession.read
       .format("geomesa")
-      .options(Map("bigtable.table.name" -> "siteexposure"))
+      .options(Map("bigtable.table.name" -> "siteexposure_1M"))
       .option("geomesa.feature", featureTypeName)
       .load()
 
@@ -42,8 +42,15 @@ object GeomesaHbaseReadUsingSparkSqlWildFire {
       "wildfireevent")
 
 
+    //val sqlQuery =
+    //  "select OBJECTID, ACRES from wildfireevent  where OBJECTID < 100 limit 10"
+
     val sqlQuery =
-      "select OBJECTID, ACRES from wildfireevent  where OBJECTID < 100 limit 10"
+          "select event.portfolio_id, sum(event.s_udf_met1), avg(event.s_udf_met1)" +
+            "from event  , wildfireevent  " +
+            "where st_intersects(event.geom , wildfireevent.geometry) group by event.portfolio_id"
+
+
 
     val resultDataFrame = sparkSession.sql(sqlQuery)
 
