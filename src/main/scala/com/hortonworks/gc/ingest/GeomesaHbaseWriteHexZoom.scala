677d9a35baf1a1696aa10a2e429c470265a42898
nagarajjayakumar <nagaraj.jayakumar@gmail.com>
2017-08-23 08:42:32 -0400

    hex zoom

---
+++ src/main/scala/com/hortonworks/gc/ingest/GeomesaHbaseWriteHexZoom.scala
@@ -0,0 +1,132 @@
+package com.hortonworks.gc.ingest
+
+import java.io.IOException
+import java.util
+import java.util.Collections
+
+import com.google.common.base.Joiner
+import com.google.common.collect.Lists
+import com.vividsolutions.jts.geom.GeometryFactory
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.geotools.data.{DataStoreFinder, DataUtilities}
+import org.geotools.factory.Hints
+import org.geotools.feature.SchemaException
+import org.geotools.feature.simple.SimpleFeatureBuilder
+import org.geotools.geometry.jts.JTSFactoryFinder
+import org.locationtech.geomesa.hbase.data.HBaseDataStore
+import org.locationtech.geomesa.spark.GeoMesaSpark
+import org.locationtech.geomesa.utils.interop.SimpleFeatureTypes
+import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
+
+import scala.collection.JavaConversions._
+
+/**
+  * Geomesa ingest for site loss analysis
+  */
+object GeomesaHbaseWriteHexZoom {
+
+  // works for latest version of Geomesa 1.3.2 + Spark 2.x
+  // spark-2.0.0/bin/spark-submit --class com.hortonworks.gc.ingest.hbase.GeomesaHbaseWrite geomesa-utils-15-1.0.jar
+
+  val dsConf = Map("bigtable.table.name" -> "hex_zoom")
+
+  var ID_COL_IDX = 0
+  var featureBuilder: SimpleFeatureBuilder = null
+  var geometryFactory: GeometryFactory = JTSFactoryFinder.getGeometryFactory
+
+  val featureName = "hexzoomevent"
+  val ingestFile =
+    "hdfs://tmp/geospatial/hexzoom/ap_hex_zoom.csv"
+
+  var attributes = Lists.newArrayList(
+    "zoom_id:java.lang.Long" ,
+    "zoom_value:java.lang.Long"
+  )
+
+  val featureType: SimpleFeatureType =
+    buildGeomesaHexZoomEventFeatureType(featureName, attributes)
+
+  val ds = DataStoreFinder
+    .getDataStore(dsConf)
+    .asInstanceOf[HBaseDataStore]
+    .createSchema(featureType)
+
+  /**
+    * This is the method to create the Simple feature.
+    * @param value
+    * @return
+    */
+  def createSimpleFeature(value: String): SimpleFeature = {
+
+    val attributes: Array[String] = value.toString.split("\\|", -1)
+
+    val simpleFeature: SimpleFeature =
+      featureBuilder.buildFeature(attributes(ID_COL_IDX))
+    // be sure to tell GeoTools explicitly that you want to use the ID you provided
+    simpleFeature.getUserData
+      .put(Hints.USE_PROVIDED_FID, java.lang.Boolean.FALSE)
+
+    var i: Int = 0
+    while (i < attributes.length) {
+      simpleFeature.setAttribute(i, attributes(i))
+      i += 1
+    }
+
+    simpleFeature
+  }
+
+  @throws(classOf[SchemaException])
+  def buildGeomesaHexZoomEventFeatureType(
+      featureName: String,
+      attributes: util.ArrayList[String]): SimpleFeatureType = {
+    val name = featureName
+    val spec = Joiner.on(",").join(attributes)
+    val featureType = DataUtilities.createType(name, spec)
+    featureType.getUserData.put(SimpleFeatureTypes.DEFAULT_DATE_KEY, "SQLDATE")
+    featureType
+  }
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf()
+    // conf.setMaster("local[3]")
+    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    conf.set("spark.kryo.registrator",
+             "org.locationtech.geomesa.spark.GeoMesaSparkKryoRegistrator")
+
+    val sc = new SparkContext(conf.setAppName("Geomesa Event Ingest"))
+
+    val distDataRDD = sc.textFile(ingestFile)
+
+    val processedRDD: RDD[SimpleFeature] = distDataRDD.mapPartitions {
+      valueIterator =>
+        if (valueIterator.isEmpty) {
+          Collections.emptyIterator
+        }
+
+        //  setup code for SimpleFeatureBuilder
+        try {
+          val featureType: SimpleFeatureType =
+            buildGeomesaHexZoomEventFeatureType(featureName, attributes)
+          featureBuilder = new SimpleFeatureBuilder(featureType)
+        } catch {
+          case e: Exception => {
+            throw new IOException("Error setting up feature type", e)
+          }
+        }
+
+        valueIterator.map { s =>
+          // Processing as before to build the SimpleFeatureType
+          val simpleFeature = createSimpleFeature(s)
+          if (!valueIterator.hasNext) {
+            // cleanup here
+          }
+          simpleFeature
+        }
+    }
+
+    GeoMesaSpark.apply(dsConf).save(processedRDD, dsConf, featureName)
+    println("""Hex Zoom Ingestion completed ...""")
+  }
+}
